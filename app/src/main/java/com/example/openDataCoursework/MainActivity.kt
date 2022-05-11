e05e887bd727e55afd6bcd2fd75dbf520d28228c
ag1g18 <ag1g18@soton.ac.uk>
2022-05-11 11:47:15 +0100

    conflict fixed

--- app/src/main/java/com/example/openDataCoursework/MainActivity.kt
+++ app/src/main/java/com/example/openDataCoursework/MainActivity.kt
@@ -38,8 +38,6 @@ import com.here.sdk.core.GeoCoordinates
 
 import com.example.openDataCoursework.PermissionsRequestor.ResultListener
 import com.here.sdk.core.engine.SDKBuildInformation
-import com.here.sdk.mapview.MapScheme
-import com.here.sdk.mapview.MapView
 
 
 const val EXTRA_MESSAGE = "com.example.openDataCoursework.MESSAGE"
@@ -49,180 +47,140 @@ const val Destination = "com.example.openDataCoursework.Destination"
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit private var mapView: MapView;
-    lateinit private var routeExample: Routing;
+    private lateinit var routeExample: Routing;
 
+        private var permissionsRequestor: PermissionsRequestor? = null
+        private var mapView: MapView? = null
+        private var searchExample: SearchExample? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            supportActionBar?.hide();// Hide ActionBar
+            setContentView(R.layout.activity_main)
+            Log.d("", "HERE SDK version: " + SDKBuildInformation.sdkVersion().versionName)
+            // Get a MapView instance from layout.
+            mapView = findViewById(R.id.map_view)
+            mapView!!.onCreate(savedInstanceState)
+            handleAndroidPermissions()
+            // These functions below need be set in the onCreat() function, important !!!
+            // Otherwise, they do not work
+            val origin = this.findViewById<EditText>(R.id.originSearch)
+            val destination = this.findViewById<EditText>(R.id.destinationSearch)
+            //在该Editview获得焦点的时候将“回车”键改为“搜索”
+            // Change the "Enter" key to "Search" when the Editview gets focus.
+            destination.imeOptions = EditorInfo.IME_ACTION_SEARCH
+            destination.inputType = EditorInfo.TYPE_CLASS_TEXT
+            origin.inputType = EditorInfo.TYPE_CLASS_TEXT
+            //设置单行输入，不然回车【搜索】会换行
+            // Set single line input, otherwise, the “Enter” key will realize the line feed function
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
-        mapView = findViewById(R.id.map_view);
-        mapView.onCreate(savedInstanceState);
-        loadMapScene();
+            // Define search function
+                    destination.isSingleLine = true
+                    var searchContextOrigin: String? = null
 
+                    // Define the Obtain function
+                    fun obtain() {
+                        searchContextOrigin = origin.text.toString()
+                        if (TextUtils.isEmpty(searchContextOrigin)) {
+                            Toast.makeText(this, "Please enter the Origin", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
 
+                    // Define the Search function
+                    fun search(searchContextOrigin: String) {
+                        val searchContextDestination: String = destination.text.toString()
+                        if (TextUtils.isEmpty(searchContextDestination) || TextUtils.isEmpty(
+                                searchContextOrigin
+                            )
+                        ) {
+                            Toast.makeText(
+                                this,
+                                "Please enter the Origin and Destination",
+                                Toast.LENGTH_SHORT
+                            ).show()
 
-    private var permissionsRequestor: PermissionsRequestor? = null
-    private var mapView: MapView? = null
-    private var searchExample: SearchExample? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        supportActionBar?.hide();// Hide ActionBar
-        setContentView(R.layout.activity_main)
-        Log.d("", "HERE SDK version: " + SDKBuildInformation.sdkVersion().versionName)
-        // Get a MapView instance from layout.
-        mapView = findViewById(R.id.map_view)
-        mapView!!.onCreate(savedInstanceState)
-        handleAndroidPermissions()
-        // These functions below need be set in the onCreat() function, important !!!
-        // Otherwise, they do not work
-        val origin = this.findViewById<EditText>(R.id.originSearch)
-        val destination = this.findViewById<EditText>(R.id.destinationSearch)
-        //在该Editview获得焦点的时候将“回车”键改为“搜索”
-        // Change the "Enter" key to "Search" when the Editview gets focus.
-        destination.imeOptions = EditorInfo.IME_ACTION_SEARCH
-        destination.inputType = EditorInfo.TYPE_CLASS_TEXT
-        origin.inputType = EditorInfo.TYPE_CLASS_TEXT
-        //设置单行输入，不然回车【搜索】会换行
-        // Set single line input, otherwise, the “Enter” key will realize the line feed function
-
-        editText.isSingleLine = true
-        // Define search function
-        fun search() {
-
-            val searchContext: String = editText.text.toString()
-            if (TextUtils.isEmpty(searchContext)) {
-                Toast.makeText(this, "The input box is empty, please enter", Toast.LENGTH_SHORT).show()
-
-        destination.isSingleLine = true
-        var searchContextOrigin: String? = null
-        // Define the Obtain function
-        fun obtain() {
-            searchContextOrigin = origin.text.toString()
-            if (TextUtils.isEmpty(searchContextOrigin)) {
-                Toast.makeText(this, "Please enter the Origin", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        // Define the Search function
-        fun search(searchContextOrigin: String) {
-            val searchContextDestination: String = destination.text.toString()
-            if (TextUtils.isEmpty(searchContextDestination) || TextUtils.isEmpty(searchContextOrigin)) {
-                Toast.makeText(this, "Please enter the Origin and Destination", Toast.LENGTH_SHORT).show()
-
-            } else {
+                        } else {
 //             Here we should input the API that we use to use the map to search the data
 //             Here is an example you can check
-                searchExample!!.originClicked(searchContextOrigin)
-                searchExample!!.destinationClicked(searchContextDestination)
+                            searchExample!!.originClicked(searchContextOrigin)
+                            searchExample!!.destinationClicked(searchContextDestination)
 //                val intent = Intent(this, ResultNavigation::class.java).apply {
 //                    putExtra(Origin, searchContextOrigin)
 //                    putExtra(Destination, searchContextDestination)
 //                }
 //                startActivity(intent)
-            }
-        }
-        //添加imeOptions的监听
-        //add the listener of imeOptions
-        destination.setOnEditorActionListener(OnEditorActionListener { textView, actionId, keyEvent ->
-            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                searchContextOrigin?.let { search(it) }
-            }
-            false
-        })
-        origin.setOnEditorActionListener(OnEditorActionListener { textView, actionId, keyEvent ->
-            obtain()
-            false
-        })
-    }
+                        }
+                    }
 
+                    //添加imeOptions的监听
+                    //add the listener of imeOptions
+                    destination.setOnEditorActionListener(OnEditorActionListener { textView, actionId, keyEvent ->
+                        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                            searchContextOrigin?.let { search(it) }
+                        }
+                        false
+                    })
+                    origin.setOnEditorActionListener(OnEditorActionListener { textView, actionId, keyEvent ->
+                        obtain()
+                        false
+                    })
+                }
+            private fun handleAndroidPermissions() {
+                permissionsRequestor = PermissionsRequestor(this)
+                permissionsRequestor!!.request(object : ResultListener {
+                    override fun permissionsGranted() {
+                        loadMapScene()
+                    }
 
+                    override fun permissionsDenied() {
+                        Log.e(TAG, "Permissions denied by user.")
+                    }
+                })
 
-
-
-
-
-
-
-
-
-
-
-    /** Called when the user taps the Send button */
-    fun Navigation(view: View) {
-        // Do something in response to button
-        val intent = Intent(this, NavigationFunction::class.java)
-        startActivity(intent)
-
-    private fun handleAndroidPermissions() {
-        permissionsRequestor = PermissionsRequestor(this)
-        permissionsRequestor!!.request(object : ResultListener {
-            override fun permissionsGranted() {
-                loadMapScene()
             }
 
-            override fun permissionsDenied() {
-                Log.e(TAG, "Permissions denied by user.")
+            override fun onRequestPermissionsResult(
+                requestCode: Int,
+                permissions: Array<String>,
+                grantResults: IntArray
+            ) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                permissionsRequestor!!.onRequestPermissionsResult(requestCode, grantResults)
             }
-        })
 
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        permissionsRequestor!!.onRequestPermissionsResult(requestCode, grantResults)
-    }
-
-    private fun loadMapScene() {
-        // Load a scene from the HERE SDK to render the map with a map scheme.
-        mapView!!.mapScene.loadScene(
-            MapScheme.NORMAL_DAY
-        ) { mapError ->
-            if (mapError == null) {
-                searchExample = SearchExample(this, mapView!!)
-            } else {
-                Log.d(TAG, "onLoadScene failed: $mapError")
+            private fun loadMapScene() {
+                mapView?.mapScene?.loadScene(
+                    MapScheme.NORMAL_DAY
+                ) { mapError ->
+                    if (mapError == null) {
+                        routeExample = Routing(this@MainActivity, mapView)
+                        // routeExample.setEnd(coord)
+                        // routeExample.setStart(coord)
+                    } else {
+                        Log.d(TAG, "Loading map failed: mapErrorCode: " + mapError.name)
+                    }
+                }
             }
-        }
-    }
 
-    private fun loadMapScene() {
-        mapView.mapScene.loadScene(
-            MapScheme.NORMAL_DAY
-        ) { mapError ->
-            if (mapError == null) {
-                routeExample = Routing(this@MainActivity, mapView)
-               // routeExample.setEnd(coord)
-               // routeExample.setStart(coord)
-            } else {
-                Log.d(TAG, "Loading map failed: mapErrorCode: " + mapError.name)
+            fun changeStartPoint(coord: GeoCoordinates?) {
+                routeExample.setStart(coord);
             }
-        }
-    }
-    fun changeStartPoint(coord: GeoCoordinates?){
-        routeExample.setStart(coord);
-    }
-    fun changeEnd(coord: GeoCoordinates?){
-        routeExample.setEnd(coord);
-    }
-    fun addExampleRoute(view: View?) {
-        routeExample.addExampleRoute()
-    }
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
 
-    fun searchExampleButtonClicked(view: View?) {
-        searchExample!!.onSearchButtonClicked()
-    }
+            fun changeEnd(coord: GeoCoordinates?) {
+                routeExample.setEnd(coord);
+            }
 
-    fun geocodeAnAddressButtonClicked(view: View?) {
-        searchExample!!.onGeocodeButtonClicked()
-    }
+            fun addExampleRoute(view: View?) {
+                routeExample.addExampleRoute()
+            }
+
+            fun searchExampleButtonClicked(view: View?) {
+                searchExample!!.onSearchButtonClicked()
+            }
+
+            fun geocodeAnAddressButtonClicked(view: View?) {
+                searchExample!!.onGeocodeButtonClicked()
+            }
 
 //    fun originSearchAchieve(origin: String?) {
 //        searchExample!!.originClicked(origin)
@@ -232,33 +190,31 @@ class MainActivity : AppCompatActivity() {
 //        searchExample!!.destinationClicked(destination)
 //    }
 
-    override fun onPause() {
-        super.onPause()
-        mapView!!.onPause()
+            override fun onPause() {
+                super.onPause()
+                mapView!!.onPause()
+            }
 
-    }
+            override fun onResume() {
+                super.onResume()
+                mapView!!.onResume()
 
-    override fun onResume() {
-        super.onResume()
+            }
 
-        mapView!!.onResume()
+            override fun onDestroy() {
+                super.onDestroy()
+                mapView!!.onDestroy()
+            }
 
-    }
+            companion object {
+                private val TAG = MainActivity::class.java.simpleName
+            }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView!!.onDestroy()
-    }
-
-    companion object {
-        private val TAG = MainActivity::class.java.simpleName
-    }
-
-    /** Called when the user taps the Send button */
-    fun clear(view: View) {
-        //FAB
-        searchExample?.clearMap()
-        }
+            /** Called when the user taps the Send button */
+            fun clear(view: View) {
+                //FAB
+                searchExample?.clearMap()
+            }
 
 
 //    fun Marked(view: View) {
@@ -271,5 +227,7 @@ class MainActivity : AppCompatActivity() {
 //        val intent = Intent(this, Settings::class.java)
 //        startActivity(intent)
 //    }
-    }
+
+
+}
 
